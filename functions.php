307a8071955c6b1f8acd9a3d3f6293348c479d64
DONObx <60740556+DONObx@users.noreply.github.com>
2020-10-12 15:18:21 +0200

    update 9

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@ function load_stylesheets()
 {
 
 
-		wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.mincss', array(), false, 'all');
+		wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
 		wp_enqueue_style('stylesheet');
 
 
 }
-add_action('wp_enqueue_scripts', 'load_stylesheets')
+add_action('wp_enqueue_scripts', 'load_stylesheets');
